f5ca5855bfb72d0e1e3771fbebee5a09a66e27ab
emil.mocan@gmail.com <emil.mocan@gmail.com>
2019-02-16 11:08:42 +0200

    Display number of threads launched

--- Cplusplus/RecursiveThreadMain/Source.cpp
+++ Cplusplus/RecursiveThreadMain/Source.cpp
@@ -1,7 +1,16 @@
+#include <iostream>
 #include <thread>
+#include <mutex>
+
+std::mutex m;
+int x = 0;
 
 int main()
 {
   std::thread t(main);
+  {
+    std::lock_guard<std::mutex> guard(m);
+    std::cout << x++ << std::endl;
+  }
   t.join();
 }
